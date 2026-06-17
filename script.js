3757454a91e75551e73e4e2077e65bba10d84a07
TousiFx <mr.trexyt9@gmail.com>
2026-06-17 13:35:44 +0530

    change

--- script.js
+++ script.js
@@ -2,15 +2,32 @@ const inputFill = document.getElementById("task-input");
 const getBtn = document.getElementById("addBtn");
 const outputFill = document.getElementById("task-list");
 
-button.addEventListener("click", function () {
+getBtn.addEventListener("click", function () {
   const inputValue = inputFill.value;
 
   if (inputValue !== "") {
     const newTodo = document.createElement("li");
     const spanTodo = document.createElement("span");
-    spanTodo.textContent = textValue;
+    spanTodo.textContent = inputValue;
+    newTodo.className = "list-line";
 
     const delBtn = document.createElement("button");
-    delBtn.value = "X";
+    delBtn.textContent = "X";
+    delBtn.className = "delete-btn";
+    newTodo.appendChild(delBtn);
+    newTodo.appendChild(spanTodo);
+
+    outputFill.appendChild(newTodo);
+    inputFill.value = "";
+  } else {
+    alert("Please enter some text first!");
+  }
+});
+
+outputFill.addEventListener("click", function (event) {
+  if (event.target.classList.contains("delete-btn")) {
+    const removeBtn = event.target.parentElement;
+
+    removeBtn.remove();
   }
 });
