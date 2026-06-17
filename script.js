c455e7d1c1e9c3540a6afaf4be2be4a3a2400dea
TousiFx <mr.trexyt9@gmail.com>
2026-06-17 12:58:30 +0530

    adas

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-function updatedText() {
-  const userInput = document.getElementById("task-input").value;
-  document.getElementById("task-list").textContent += userInput;
-}
+const inputFill = document.getElementById("task-input");
+const getBtn = document.getElementById("addBtn");
+const outputFill = document.getElementById("task-list");
+
+button.addEventListener("click", function () {
+  const inputValue = inputFill.value;
+
+  if (inputValue !== "") {
+    const newTodo = document.createElement("li");
+    const spanTodo = document.createElement("span");
+    spanTodo.textContent = textValue;
+
+    const delBtn = document.createElement("button");
+    delBtn.value = "X";
+  }
+});
